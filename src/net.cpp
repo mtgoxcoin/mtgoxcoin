22a8db4187ac645bb179a1ffac11d09b986edd6c
MtGox Coin <info@mtgoxcoin.com>
2014-03-09 22:15:35 -0500

    updated dns seed ip

--- src/net.cpp
+++ src/net.cpp
@@ -1192,12 +1192,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"seed.mtgoxcoin.com", "seed.mtgoxcoin.com"},
+    {"76.74.178.191", "76.74.178.191"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"seed.mtgoxcoin.com", "seed.mtgoxcoin.com"},
+    {"76.74.178.191", "76.74.178.191"},
     {NULL, NULL}
 };
 
